3c022e45a3e886990844df0a4a5ad3197ef3d063
Jahmal Lewis <Jahmal23@gmail.com>
2016-01-20 17:47:34 -0500

    able to open whitepages hm page

--- Gemfile
+++ Gemfile
@@ -3,12 +3,11 @@ source "https://rubygems.org"
 
 require 'rubygems'
 require 'bundler/setup'
-require 'nokogiri'
 
 
 gem 'rspec', '~>3.4'
 gem 'rubocop', require: false
-
+gem "nokogiri", '1.6.7.2', :require => "nokogiri"
 
 
 #gem "rails"
--- Lib/loader.rb
+++ Lib/loader.rb
@@ -1,12 +1,22 @@
 
+require 'nokogiri'
+require 'open-uri'
+
 #This class is responsible for any whitepages http requests and passing back any downloaded content
 
 class Loader
 
+    BASE_WHITE_PAGES_URL = "http://www.whitepages.com/"
+
     def get_content
 
-      '<html><body>junk</body></html>'
+      page = Nokogiri::HTML(open(BASE_WHITE_PAGES_URL))
 
+      #'<html><body>junk</body></html>'
+
+      page.css('body.home-page').each do |node|
+        puts node.text
+      end
     end
 
 end
