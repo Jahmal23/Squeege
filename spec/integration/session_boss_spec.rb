e51c9501cd6546eb47ffc92b6c6882923d5fe95b
Jahmal Lewis <Jahmal23@gmail.com>
2016-04-26 16:48:59 -0400

    almost got you rubocop

--- spec/integration/session_boss_spec.rb
+++ spec/integration/session_boss_spec.rb
@@ -4,13 +4,9 @@ require_relative '../../Lib/home_page'
 
 
 describe 'Create capybara session' do
-
   it 'should have a valid session' do
-
     sess = SessionBoss.new
 
     expect(sess.capy_session.nil?).to eq(false)
-
   end
-
 end
