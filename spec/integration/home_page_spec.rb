4224eb728c41a5675d6dedfdabc20075080523c5
Jahmal Lewis <Jahmal23@gmail.com>
2016-04-26 16:48:29 -0400

    almost got you rubocop

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -3,29 +3,22 @@ require_relative '../../Lib/home_page'
 require_relative '../../Lib/session_boss'
 
 describe 'White pages home page object' do
-
   let(:capy_sess) { SessionBoss.new.capy_session }
 
   it 'should perform a basic search' do
-
     home = HomePage.new
-    current_url = home.perform_search(capy_sess,'Oliveira','30062')
+    current_url = home.perform_search(capy_sess, 'Oliveira', '30062')
 
     expect(current_url.nil?).to eq(false)
-    expect(current_url.include? 'Oliveira').to eq(true)
-    expect(current_url.include? '30062').to eq(true)
-
+    expect(current_url.include?('Oliveira')).to eq(true)
+    expect(current_url.include?('30062')).to eq(true)
   end
 
   it 'should go to the results page' do
-
     home = HomePage.new
-    current_url = home.perform_search(capy_sess,'Oliveira','30062')
+    current_url = home.perform_search(capy_sess, 'Oliveira', '30062')
 
-
-    expect(current_url.include? 'Oliveira').to eq(true)
-    expect(current_url.include? '30062').to eq(true)
-
+    expect(current_url.include?('Oliveira')).to eq(true)
+    expect(current_url.include?('30062')).to eq(true)
   end
-
 end
