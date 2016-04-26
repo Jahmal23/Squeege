f82212fe39385c937d8322e6fcb07abcc718d636
Jahmal Lewis <Jahmal23@gmail.com>
2016-04-26 16:40:47 -0400

    dang you rubocop

--- Lib/session_boss.rb
+++ Lib/session_boss.rb
@@ -2,23 +2,19 @@ require 'capybara/poltergeist'
 
 Capybara.default_driver = :poltergeist
 Capybara.register_driver :poltergeist do |app|
-  options = {
-      :js_errors => false,
-      :timeout => 120,
-      :debug => false,
-      :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
-      :inspector => true,
-  }
+  options = {  js_errors: false,
+               timeout: 120,
+               debug: false,
+               phantomjs_options: ['--load-images=no', '--disk-cache=false'],
+               inspector: true }
   Capybara::Poltergeist::Driver.new(app, options)
 end
 
 # Manages instances of Capybara
 class SessionBoss
-
   attr_reader :capy_session
 
   def initialize
     @capy_session = Capybara::Session.new(:poltergeist)
   end
-
 end
